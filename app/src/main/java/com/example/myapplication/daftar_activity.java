2cb62656135f4cd9c7da898ae6349c5d819aff2d
RaihanBaihaqi780 <raihanbaihaqi.2021@student.uny.ac.id>
2023-09-04 10:21:09 +0700

    add existing file

---
+++ app/src/main/java/com/example/myapplication/daftar_activity.java
@@ -0,0 +1,80 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		daftar
+	 *	@date 		Sunday 03rd of September 2023 03:27:53 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.myapplication.;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class daftar_activity extends Activity {
+
+	
+	private View _bg__daftar;
+	private TextView masukkan_nama_pengguna_anda;
+	private TextView masukkan_email_atau_nomor_telelpon_anda;
+	private TextView masukkan_kata_sandi_anda;
+	private TextView daftar;
+	private TextView daftar_ek1;
+	private TextView nama_pengguna;
+	private TextView email_atau_no__telepon;
+	private TextView kata_sandi;
+	private TextView sudah_punya_akun_;
+	private ImageView rectangle;
+	private ImageView combined_shape;
+	private ImageView rectangle_ek1;
+	private ImageView wifi;
+	private ImageView mobile_signal;
+	private View indicator;
+	private ImageView _9_41;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.daftar);
+
+		
+		_bg__daftar = (View) findViewById(R.id._bg__daftar);
+		masukkan_nama_pengguna_anda = (TextView) findViewById(R.id.masukkan_nama_pengguna_anda);
+		masukkan_email_atau_nomor_telelpon_anda = (TextView) findViewById(R.id.masukkan_email_atau_nomor_telelpon_anda);
+		masukkan_kata_sandi_anda = (TextView) findViewById(R.id.masukkan_kata_sandi_anda);
+		daftar = (TextView) findViewById(R.id.daftar);
+		daftar_ek1 = (TextView) findViewById(R.id.daftar_ek1);
+		nama_pengguna = (TextView) findViewById(R.id.nama_pengguna);
+		email_atau_no__telepon = (TextView) findViewById(R.id.email_atau_no__telepon);
+		kata_sandi = (TextView) findViewById(R.id.kata_sandi);
+		sudah_punya_akun_ = (TextView) findViewById(R.id.sudah_punya_akun_);
+		rectangle = (ImageView) findViewById(R.id.rectangle);
+		combined_shape = (ImageView) findViewById(R.id.combined_shape);
+		rectangle_ek1 = (ImageView) findViewById(R.id.rectangle_ek1);
+		wifi = (ImageView) findViewById(R.id.wifi);
+		mobile_signal = (ImageView) findViewById(R.id.mobile_signal);
+		indicator = (View) findViewById(R.id.indicator);
+		_9_41 = (ImageView) findViewById(R.id._9_41);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
